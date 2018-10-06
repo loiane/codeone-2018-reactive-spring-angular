9ac75c62b93b8f8d7b06626a3099037832ca8c55
Loiane <loianeg@gmail.com>
2018-10-06 14:22:33 -0400

    first commit

---
+++ spring-shopping-cart/src/main/java/com/loiane/springshoppingcart/SpringShoppingCartApplication.java
@@ -0,0 +1,12 @@
+package com.loiane.springshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShoppingCartApplication.class, args);
+	}
+}
