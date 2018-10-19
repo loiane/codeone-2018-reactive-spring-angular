17b949ed6a83d9416a9e0c72eab1475be992ee04
Loiane <loianeg@gmail.com>
2018-10-19 13:33:54 -0400

    demo

--- angular-shopping-cart/src/app/products/services/product.service.ts
+++ angular-shopping-cart/src/app/products/services/product.service.ts
@@ -8,21 +8,24 @@ import { Product } from '../model/product';
   providedIn: 'root'
 })
 export class ProductService {
-
   products: any[] = [];
 
   private readonly BASE_URL = environment.baseUrl;
 
   private readonly API = `${this.BASE_URL}/products`;
 
+  private readonly isLocal = environment.isLocal;
+
   constructor(private http: HttpClient) {}
 
   load() {
-    // return this.http.get<Product[]>(this.API);
-    for (let num = 1; num <= 10; num += 1) {
-      this.addProducts(num);
+    if (this.isLocal) {
+      for (let num = 1; num <= 10; num += 1) {
+        this.addProducts(num);
+      }
+      return of(this.products);
     }
-    return of(this.products);
+    return this.http.get<Product[]>(this.API);
   }
 
   private addProducts(i) {
@@ -32,12 +35,8 @@ export class ProductService {
       status: ['', '', '', 'sale'][Math.floor(Math.random() * 4)],
       discounted: ['', '', '', 'discounted'][Math.floor(Math.random() * 4)],
       discount: (Math.random() * (0.0 - 10.0) + 10.0).toFixed(2),
-      name: [
-        'Coffee'
-      ][Math.floor(Math.random() * 1)],
-      description: ['B & W', 'Grey', 'Black', 'Green', 'Black'][
-        Math.floor(Math.random() * 5)
-      ],
+      name: ['Coffee'][Math.floor(Math.random() * 1)],
+      description: ['B & W', 'Grey', 'Black', 'Green', 'Black'][Math.floor(Math.random() * 5)],
       image: i
     });
   }
--- angular-shopping-cart/src/environments/environment.prod.ts
+++ angular-shopping-cart/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: '/api'
+  baseUrl: '/api',
+  isLocal: false
 };
